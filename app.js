0e14bbd30ac4564bd2f1fab0b31ce23bf7da9495
Eliud Gateri <egateri@gmail.com>
2023-05-03 17:31:46 +0300

    initial app set up

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+ * App.js
+ * Copyright(c) 2023 Moringa School Group 1 - Eliud, Geofrey, Hellen & Kelvin
+ * MIT Licensed
+ */
+const express = require("express");
+const PORT = 5500;
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.listen(PORT, () => {
+  console.log("Server listening on port: " +PORT);
+});
