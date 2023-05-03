7aeda976ed436ceb2705479c3e27059a56117abb
Eliud Gateri <egateri@gmail.com>
2023-05-03 18:02:09 +0300

    Add the signup middleware

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const PORT = 5500;
 const app = express();
 const homeRouter = require("./routes/home");
 const loginRouter = require("./routes/login");
+const signUpRouter = require("./routes/signup");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(homeRouter);
 app.use(loginRouter);
+app.use(signUpRouter);
 app.listen(PORT, () => {
   console.log("Server listening on port: " +PORT);
 });
